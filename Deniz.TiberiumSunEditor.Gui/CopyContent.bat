fb924a8451718eea754167d05ef1dad4ce9d1634
Deniz Esen <esen@other.ch>
2024-08-04 07:35:29 +0200

    remove unused platform runtimes to reduce release size

--- Deniz.TiberiumSunEditor.Gui/CopyContent.bat
+++ Deniz.TiberiumSunEditor.Gui/CopyContent.bat
@@ -3,3 +3,9 @@ REM Parameter 1 is the source folder
 REM Parameter 2 is the target folder
 xcopy "%1Bitmaps" "%2Bitmaps" /E /Y /I
 xcopy "%1Snippets" "%2Snippets" /E /Y /I
+
+rd "%2runtimes\linux-arm64" /S /Q
+rd "%2runtimes\linux-musl-x64" /S /Q
+rd "%2runtimes\linux-x64" /S /Q
+rd "%2runtimes\osx-arm64" /S /Q
+rd "%2runtimes\osx-x64" /S /Q
