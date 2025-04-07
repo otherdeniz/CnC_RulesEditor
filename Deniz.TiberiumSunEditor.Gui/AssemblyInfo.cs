2cb93d1e247a35e9a36e3051d1fbe396c5cf29d4
Deniz Esen <esen@other.ch>
2025-04-07 14:47:05 +0200

    start of 2.6.59

--- Deniz.TiberiumSunEditor.Gui/AssemblyInfo.cs
+++ Deniz.TiberiumSunEditor.Gui/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 [assembly: System.Runtime.Versioning.TargetPlatform("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("Windows7.0")]
 
-[assembly: AssemblyFileVersion("2.6.58.0")]
-[assembly: AssemblyVersion("2.6.58.0")]
-[assembly: AssemblyInformationalVersion("2.6.58")]
+[assembly: AssemblyFileVersion("2.6.59.0")]
+[assembly: AssemblyVersion("2.6.59.0")]
+[assembly: AssemblyInformationalVersion("2.6.59")]
